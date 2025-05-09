ed38e7d8233a5459f8d9b5165f45e52817033274
gustavo <gustavo.lima04@escola.pr.gov.br>
2025-05-09 12:17:05 -0300

    Atualização 3

---
+++ Atividade/Atividade 2.py
@@ -0,0 +1,19 @@
+
+total1 = 3 + (0.75 * 60) + ((24.95 - 24.95 * 0.4) * 60)
+
+mensagem = 'O custo das 60 capas de livos será', total1
+
+print(mensagem)
+
+fretedoprimeiroexemplar = 3
+fretedosoutrosexemplares = 0.75
+preçodolivro = 24.95
+desconto = 0.4
+preçodolivrocomdesconto = 14.97
+quantidadedelivros = 60
+
+total2 = fretedoprimeiroexemplar + (fretedosoutrosexemplares * quantidadedelivros) + ((preçodolivro - preçodolivro * desconto) * quantidadedelivros)
+
+mensagem2 = 'O custo das 60 capas de livos será', total2
+
+print(mensagem2)
