e04bed92a73cf08172c45940872f2e3acf5c6e72
gustavo <gustavo.lima04@escola.pr.gov.br>
2025-05-09 11:43:23 -0300

    atualização 4

---
+++ Atividade/Atividade 1.py
@@ -0,0 +1,12 @@
+total1 = (4/3) * 3.14 * 5 ** 3
+
+print(total1)
+
+primeirotermo = 4/3
+PI = 3.14
+R = 5
+raio = 5 ** 3
+
+total2 = primeirotermo * PI * raio
+
+print('o volume da esfera equivale à', total2)
