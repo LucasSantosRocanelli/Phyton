95ceb68ebe38850fe84bae674c21a88d7f4fede2
endrius <endrius.pontes@escola.pr.gov.br>
2025-05-30 09:59:54 -0300

    atualização 7

---
+++ 3 capitulo/exemplo 1 (funções).py
@@ -0,0 +1,5 @@
+#se uma função receber uma função que ela não deve ela vai dasr err0
+#uma função deve ter parenteses se não é () e não " "
+#uma função tem um nome, e esse nome faz a função puxar informações e dar um valor
+
+string = '12'
