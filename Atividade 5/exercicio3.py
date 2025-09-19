dc1ac89dfc18ca17877ad7a989e4b13114d36737
Ana Bea <ana.santos.ribeiro01@escola.pr.gov.br>
2025-09-19 12:14:21 -0300

    Atividade 5

---
+++ Atividade 5/exercicio3.py
@@ -0,0 +1,21 @@
+def calculaQuantidadrPares(numero1, numero2, numero3, numero4):
+    quantidadePar = 0
+    totalPares = 0
+
+    if(numero1 %2 == 0)
+        quantidadePar += 1
+        totalPares += numero1
+
+
+            if (numero2 % 2 == 0)
+                quantidadePar += 2
+                totalPares += numero2
+
+                if (numero3 % 2 == 0)
+                    quantidadePar += 3
+                    totalPares += numero3
+
+ if (numero4 %2 == 0)
+        quantidadePar += 4
+        totalPares += numero4
+
