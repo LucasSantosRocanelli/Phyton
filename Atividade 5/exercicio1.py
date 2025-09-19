2b174e2ec7f068a232e45abe30679d90dc466c4d
Ana Bea <ana.santos.ribeiro01@escola.pr.gov.br>
2025-09-19 11:40:59 -0300

    Atividade 5

---
+++ Atividade 5/exercicio1.py
@@ -0,0 +1,10 @@
+def validaIdade(idade):
+   if(idade >= 18):
+       print ('voçe é maior de idade')
+
+
+idade = int(input('digite a sua idade'))
+
+
+validaIdade(idade)
+
---
+++ Atividade 5/exercicio2.py
@@ -0,0 +1,13 @@
+def numeroValidade(numero):
+    if(numero > 0):
+      elevado = (numero ** 2)
+      (print('seu numero e maior que zero'))
+
+    if(numero < 0):
+      print("Seu número é negativo")
+
+numero = int(input('Digite seu numero'))
+
+numeroValidade(numero)
+
+
