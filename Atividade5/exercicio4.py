204d068bf4029e7173e33a682070f9446aa4a722
Andrey <Andrey.schmitz@escola.pr.gov.br>
2025-10-10 09:59:37 -0300

    exercicio5

--- Atividade5/exercicio4.py
+++ Atividade5/exercicio4.py
@@ -1,28 +1,28 @@
-def ApresentaJogador(opção):
-    match opção:
+def ApresentaJogador(opcao):
+    match opcao:
 
-case1:
-    print(f"Jogador 1: Alisson")
-case2:
-    print(f"Jogador 2: Wesley")
-case3:
-    print(f"Jogador 3: Gabriel.G")
-case4:
-    print(f"Jogador 4: Marquinhos")
-case5:
-    print(f"Jogador 5: Casemiro")
-case6:
-    print(f"Jogador 6: Douglas")
-case7:
-    print(f"Jogador 7: João Pedro")
-case8:
-    print(f"Jogador 8: Bruno.G")
-case9:
-    print(f"Jogador 9: Richarlison")
-case10:
-    print(f"Jogador 10: Rafinha")
-case11:
-    print(f"Jogador 11: Paquetá")
+        case 1:
+            print(f"Jogador 1: Alisson")
+        case 2:
+             print(f"Jogador 2: Wesley")
+        case 3:
+             print(f"Jogador 3: Gabriel.G")
+        case 4:
+            print(f"Jogador 4: Marquinhos")
+        case 5:
+            print(f"Jogador 5: Casemiro")
+        case 6:
+            print(f"Jogador 6: Douglas")
+        case 7:
+            print(f"Jogador 7: João Pedro")
+        case 8:
+            print(f"Jogador 8: Bruno.G")
+        case 9:
+            print(f"Jogador 9: Richarlison")
+        case 10:
+            print(f"Jogador 10: Rafinha")
+        case 11:
+            print(f"Jogador 11: Paquetá")
 
-def (main):
+def default(main):
     print("digite o número da camisa de 1 até 11:")
---
+++ Atividade5/exercicio5.py
@@ -0,0 +1,14 @@
+def LetradoImetro(opcao):
+    match opcao:
+
+case 1:
+    print(f"Letra A do Imetro")
+case 2:
+    print(f"Letra B do Imetro")
+case 3:
+    print(f"Letra C do Imetro")
+case 4:
+    print(f"Letra D do Imetro")
+
+def (main):
+    print("")
