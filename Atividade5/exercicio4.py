2a796f457ba18fd7f444f8675f82f4886ff72af7
Andrey <Andrey.schmitz@escola.pr.gov.br>
2025-10-10 08:27:08 -0300

    exercicio4

---
+++ Atividade5/exercicio4.py
@@ -0,0 +1,28 @@
+def ApresentaJogador(opção):
+    match opção:
+
+case1:
+    print(f"Jogador 1: Alisson")
+case2:
+    print(f"Jogador 2: Wesley")
+case3:
+    print(f"Jogador 3: Gabriel.G")
+case4:
+    print(f"Jogador 4: Marquinhos")
+case5:
+    print(f"Jogador 5: Casemiro")
+case6:
+    print(f"Jogador 6: Douglas")
+case7:
+    print(f"Jogador 7: João Pedro")
+case8:
+    print(f"Jogador 8: Bruno.G")
+case9:
+    print(f"Jogador 9: Richarlison")
+case10:
+    print(f"Jogador 10: Rafinha")
+case11:
+    print(f"Jogador 11: Paquetá")
+
+def (main):
+    print("digite o número da camisa de 1 até 11:")
